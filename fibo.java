9b99a138a1f0a31378838e8fc18c1bb33ed5f934
NidhiChandarana <83494760+NidhiChandarana@users.noreply.github.com>
2021-10-29 09:40:16 +0530

    Update fibo.java

--- fibo.java
+++ fibo.java
@@ -3,7 +3,8 @@ public static void main(String args[])
 {    
  System.out.println("Printing first ten numbers from Fibonacci Series");
  int n1=0,n2=1,n3,i,count=10;    
- System.out.println(n1+ \n +n2);   
+ System.out.println(n1);  
+ System.out.println(n2);
     
  for(i=2;i<count;++i) 
  {    
