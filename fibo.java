c47f35aceac8c1d0cb84a4d11fc96cc507aa7ad3
NidhiChandarana <83494760+NidhiChandarana@users.noreply.github.com>
2021-10-29 09:41:16 +0530

    Update fibo.java

--- fibo.java
+++ fibo.java
@@ -9,7 +9,7 @@ public static void main(String args[])
  for(i=2;i<count;++i) 
  {    
   n3=n1+n2;    
-  System.out.println(" "+n3);    
+  System.out.println(n3);    
   n1=n2;    
   n2=n3;    
  }    
