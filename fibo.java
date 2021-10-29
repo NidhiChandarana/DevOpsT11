6664683f1514f79a6c7cfdefed786fa1f0a903f1
NidhiChandarana <83494760+NidhiChandarana@users.noreply.github.com>
2021-10-29 10:11:59 +0530

    Update fibo.java

--- fibo.java
+++ fibo.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 class fibo{  
 public static void main(String args[])  
 {    
- Scanner myObj = new Scanner(System.in);
- System.out.println("Enter value n : ");
- int n = myObj.nextInt();  
- System.out.println("Printing first"+n+"numbers from Fibonacci Series");
- int n1=0,n2=1,n3,i,count=n;    
+//  Scanner myObj = new Scanner(System.in);
+//  System.out.println("Enter value n : ");
+//  int n = myObj.nextInt();  
+ System.out.println("Printing first ten numbers from Fibonacci Series");
+ int n1=0,n2=1,n3,i,count=10;    
  System.out.println(n1);  
  System.out.println(n2);
     
